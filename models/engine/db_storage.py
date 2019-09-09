aa18f3fc806c1922500d6635a8af61e70d5efbf5
scurry222 <scurry222@gmail.com>
2019-09-09 20:08:06 +0000

    add get and count implementation

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,15 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """retrieve one object"""
+        if self.all(cls) is not None:
+            return self.all(cls).get(cls + '.' + id)
+
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        if cls:
+            return len(self.all(cls))
+        else:
+            return len(self.all())
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,15 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieve one object"""
+        if self.all(cls) is not None:
+            return self.all(cls).get(cls + '.' + id)
+
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        if cls:
+            return len(self.all(cls))
+        else:
+            return len(self.all())
