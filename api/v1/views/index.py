6c3feafe944ef3e4768d96db4115533ac1f88cad
jmtes <juliennetesoro@gmail.com>
2019-09-09 22:31:55 +0000

    Add stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
+''' Define route for app views.
+
+    Attributes:
+        json_check - Return page status.
+'''
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
 
 @app_views.route('/status')
 def json_check():
+    ''' Return page status. '''
     return jsonify(status='OK')
+
+
+@app_views.route('/stats')
+def count_objects():
+    ''' Return count of objects by type. '''
+    return jsonify(amenities=storage.count('Amenity'),
+                   cities=storage.count('City'),
+                   places=storage.count('Place'),
+                   reviews=storage.count('Review'),
+                   states=storage.count('State'),
+                   users=storage.count('User'))
