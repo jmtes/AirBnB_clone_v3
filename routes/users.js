73d206e969e1af2dcb01ae5ab1794446b478b8b8
jmtes <juliennetesoro@gmail.com>
2020-06-30 19:21:14 -0700

    Implement user registration and retrieval

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+
+const keys = require('../config/keys');
 
 const router = express.Router();
 
@@ -7,15 +14,85 @@ const router = express.Router();
 // @access  Public
 router.get('/:id', async (req, res) => {
   const id = req.params.id;
-  res.send(`GET user with id ${id}`);
+
+  try {
+    const user = await User.findById(id);
+
+    if (!user) {
+      res.status(404).json({ message: 'User not found.' });
+    } else {
+      res.json({
+        name: user.name,
+        avatar: user.avatar,
+        places: user.places
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Something went wrong. Try again later.' });
+  }
 });
 
 // @route   POST /api/users
 // @desc    Register new user
 // @access  Public
-router.post('/', async (req, res) => {
-  res.send(`POST register new user`);
-});
+router.post(
+  '/',
+  [
+    check('name', 'Please provide a name.').not().isEmpty(),
+    check('email', 'Please provide a valid email.').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 8 or more characters.'
+    ).isLength({ min: 8 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    let user;
+
+    try {
+      user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(409)
+          .json({ message: 'An account with that email already exists.' });
+      }
+
+      user = new User({
+        name,
+        email
+      });
+
+      const salt = await bcrypt.genSalt();
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(payload, keys.jwtSecret, { expiresIn: 240000 }, (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
+    } catch (err) {
+      console.log(err.message);
+      res
+        .status(500)
+        .json({ message: 'Something went wrong. Try again later.' });
+    }
+  }
+);
 
 // @route   PUT /api/users/:id
 // @desc    Edit user info
