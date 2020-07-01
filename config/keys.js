74ee16cafd935b1c40c994eb845bd26d0e3b2030
jmtes <juliennetesoro@gmail.com>
2020-06-30 19:20:22 -0700

    Add JWT secret

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: `mongodb+srv://${process.env.BEARBNB_DB_USER}:${process.env.BEARBNB_DB_PWD}@bearbnb-pztuf.mongodb.net/test?retryWrites=true&w=majority`
+  mongoURI: `mongodb+srv://${process.env.BEARBNB_DB_USER}:${process.env.BEARBNB_DB_PWD}@bearbnb-pztuf.mongodb.net/test?retryWrites=true&w=majority`,
+  jwtSecret: process.env.BEARBNB_JWT_SECRET
 };
