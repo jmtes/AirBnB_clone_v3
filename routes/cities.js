fd0bbc4e37957e4813d4575c01e17dfb2efec7d9
jmtes <juliennetesoro@gmail.com>
2020-07-03 14:43:58 -0700

    Remove POST /api/cities endpoint
    I figured, since cities would only ever be created in the circumstance that a place was created in a city that doesn't yet exist in the DB, it would make little sense to have it a city creation endpoint in the API.
    Thus, I decided to make a utility function for city creation and have it be an internal implementation detail of place creation.

--- routes/cities.js
+++ routes/cities.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
+const City = require('../models/City');
+
 const router = express.Router();
 
 // @route   GET api/cities
 // @desc    Get all cities
 // @access  Public
 router.get('/', async (req, res) => {
+  console.log(req.query);
   res.send('GET all cities');
 });
 
@@ -23,13 +26,6 @@ router.get('/:id', async (req, res) => {
 router.get('/search/:q', async (req, res) => {
   const { q } = req.params;
   res.send(`GET cities matching ${q}`);
-});
-
-// @route   POST /api/cities
-// @desc    Add city to database
-// @access  Private
-router.post('/', async (req, res) => {
-  res.send('POST new city');
 });
 
 module.exports = router;
