b2e4a9f2ebc20f8080057b38951a20176fd65785
jmtes <juliennetesoro@gmail.com>
2020-07-03 13:39:31 -0700

    Implement place creation

--- routes/places.js
+++ routes/places.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+const axios = require('axios');
+
+const authCheck = require('../middleware/authCheck');
+const keys = require('../config/keys');
+
+const City = require('../models/City');
+const Place = require('../models/Place');
 
 const router = express.Router();
 
@@ -21,9 +29,107 @@ router.get('/:id', async (req, res) => {
 // @route   POST /api/places
 // @desc    Add place to database
 // @access  Private
-router.post('/', async (req, res) => {
-  res.send(`POST place`);
-});
+router.post(
+  '/',
+  [
+    authCheck,
+    check('name', 'Please provide a name for your place.').isString(),
+    check('desc', 'Please provide a description for your place.').isString(),
+    check('address', 'Please provide an address for your place.').isString(),
+    check('beds', 'Please provide the number of beds.').isInt(),
+    check('baths', 'Please provide the number of baths.').isInt(),
+    check(
+      'price',
+      'Please provide the price per night for your place.'
+    ).isNumeric()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+
+    // Validate address with Location IQ
+    const locRes = await axios.get('https://us1.locationiq.com/v1/search.php', {
+      params: {
+        q: req.body.address,
+        format: 'json',
+        addressdetails: 1,
+        limit: 1,
+        key: keys.locationIQAPIKey
+      }
+    });
+
+    const locData = locRes.data[0];
+
+    // Make sure place is residential
+    if (locData.class !== 'place' || locData.type !== 'house') {
+      res.status(400).json({ message: 'Please provide a valid address.' });
+      return;
+    }
+
+    // Check if city exists
+    let city = await City.findOne({
+      name: locData.address.city,
+      state: locData.address.state || '',
+      region: locData.address.region || '',
+      country: locData.address.country
+    });
+
+    if (!city) {
+      // Get city coordinates
+      const cityRes = await axios.get(
+        'https://us1.locationiq.com/v1/search.php',
+        {
+          params: {
+            city: locData.address.city,
+            state: locData.address.state || '',
+            region: locData.address.region || '',
+            country: locData.address.country || '',
+            format: 'json',
+            addressdetails: 1,
+            limit: 1,
+            key: keys.locationIQAPIKey
+          }
+        }
+      );
+
+      const { lat, lon } = cityRes.data[0];
+
+      const newCity = new City({
+        name: locData.address.city,
+        state: locData.address.state || '',
+        region: locData.address.region || '',
+        country: locData.address.country,
+        latitude: lat,
+        longitude: lon
+      });
+
+      city = await newCity.save();
+    }
+
+    const newPlace = new Place({
+      name: req.body.name,
+      desc: req.body.desc,
+      ownerID: req.user.id,
+      cityID: city._id,
+      address: `${locData.address.house_number} ${locData.address.road}`,
+      latitude: locData.lat,
+      longitude: locData.lon,
+      beds: req.body.beds,
+      baths: req.body.baths,
+      price: req.body.price,
+      maxGuests: req.body.maxGuests,
+      amenities: req.body.amenities,
+      photos: req.body.photos
+    });
+
+    const place = await newPlace.save();
+
+    res.json(place);
+  }
+);
 
 // @route   PUT api/places/:id
 // @desc    Edit place details
