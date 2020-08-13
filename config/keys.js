fc3266f8b96ecbfbbdbf8f9eaad58a3c4682bae8
jmtes <juliennetesoro@gmail.com>
2020-08-12 18:38:40 -0700

    Change Mongo URI value

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: `mongodb+srv://${process.env.BEARBNB_DB_USER}:${process.env.BEARBNB_DB_PWD}@bearbnb-pztuf.mongodb.net/test?retryWrites=true&w=majority`,
+  mongoURI: process.env.BEARBNB_DB_URI,
   jwtSecret: process.env.BEARBNB_JWT_SECRET,
   locationIQAPIKey: process.env.BEARBNB_LOCATIONIQ_API_KEY,
   unsplashAccessKey: process.env.BEARBNB_UNSPLASH_ACCESS_KEY
