ef4426a42280f16cbdea14e18dbc80a2c3ea3303
jmtes <juliennetesoro@gmail.com>
2020-09-12 21:19:28 -0700

    Add endpoint for getting a single city

--- routes/cities/handlers.js
+++ routes/cities/handlers.js
@@ -1,4 +1,26 @@
 const City = require('../../models/City');
+const Place = require('../../models/Place');
+
+const getCity = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const city = await City.findById(id);
+
+    if (!city) {
+      res.status(404).json({ message: 'City not found.' });
+      return;
+    }
+
+    const places = await Place.find({ cityID: id });
+    city.places = places;
+
+    res.json(city);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: 'Something went wrong. Try again later.' });
+  }
+};
 
 const getCities = async (req, res) => {
   try {
@@ -11,4 +33,4 @@ const getCities = async (req, res) => {
   }
 };
 
-module.exports = { getCities };
+module.exports = { getCity, getCities };
