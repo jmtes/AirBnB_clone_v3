c29466507f62a38978c3b10423032f6798af25e9
jmtes <juliennetesoro@gmail.com>
2020-09-30 22:53:54 -0700

    Set up GraphQL server and Prisma client

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import { GraphQLServer } from 'graphql-yoga';
+
+import { resolvers, fragmentReplacements } from './resolvers';
+import prisma from './prisma';
+
+// Initialize server
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: (req) => ({ req, prisma }),
+  fragmentReplacements
+});
+
+export default server;
