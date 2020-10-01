81a82f22a6be82e7c43e48c119a1c9499123f261
jmtes <juliennetesoro@gmail.com>
2020-09-30 22:56:05 -0700

    Use GraphQL server

--- src/index.js
+++ src/index.js
@@ -1,36 +1,9 @@
-import express from 'express';
-import connectDB from '../config/db';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
-// Import routes
-import cityRoutes from '../routes/cities';
-import placeRoutes from '../routes/places';
-import reviewRoutes from '../routes/reviews';
-import reservationRoutes from '../routes/reservations';
-import userRoutes from '../routes/users';
-import authRoutes from '../routes/auth';
+import server from './server';
 
-const app = express();
-
-// Initialize middleware
-app.use(express.json({ extended: false }));
-
-// Connect to MongoDB
-connectDB();
-
-// Set routes
-app.use('/api/cities', cityRoutes);
-app.use('/api/places', placeRoutes);
-app.use('/api/reviews', reviewRoutes);
-app.use('/api/reservations', reservationRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/auth', authRoutes);
-
-const PORT = process.env.BEARBNB_PORT || 5000;
-
-app.get('/', (req, res) => {
-  res.send('Hello');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server started on Port ${PORT}`);
+// Start server
+server.start({ port: process.env.PORT }, () => {
+  console.log(`Server listening on Port ${process.env.PORT}`);
 });
