e1bee7b0fe4f52f91ccf65f09437a15bfbba16c9
jmtes <juliennetesoro@gmail.com>
2020-10-06 15:23:49 -0700

    Add validators for listing fields

--- src/resolvers/utils/validation.js
+++ src/resolvers/utils/validation.js
@@ -1,5 +1,6 @@
 import validator from 'validator';
 import xss from 'xss';
+import axios from 'axios';
 
 const validateEmail = (email) => {
   const emailIsValid = validator.isEmail(email);
@@ -42,9 +43,72 @@ const validateBio = (bio) => {
   return sanitizedBio;
 };
 
+const validateDesc = (desc) => {
+  // Trim extra whitespace and escape HTML entities
+  const sanitizedDesc = xss(validator.trim(desc));
+
+  // Make sure description is not empty
+  if (sanitizedDesc.length < 1) throw Error('Description may not be empty.');
+
+  // Make sure word count does not exceed 250
+  const wordCount = desc.split(/\s/).length;
+  if (wordCount > 250) throw Error('Description may not exceed 250 words.');
+
+  return sanitizedDesc;
+};
+
+const validateAddress = async (address) => {
+  console.log(address);
+
+  try {
+    // Make sure address contains building number
+    const [buildingNumber] = address.split(' ', 1);
+    if (Number.isNaN(parseInt(buildingNumber, 10))) throw Error();
+
+    console.log('Address contains building number');
+
+    // Validate address with Location IQ
+    const { data } = await axios.get(
+      'https://us1.locationiq.com/v1/search.php',
+      {
+        params: {
+          q: address,
+          format: 'json',
+          addressdetails: 1,
+          limit: 1,
+          key: process.env.LOCATION_IQ_API_KEY
+        }
+      }
+    );
+
+    const [location] = data;
+
+    // Sometimes, the LocationIQ response will not include a house number.
+    // This sets the house number to the previously parsed building number.
+    if (!location.address.house_number)
+      location.address.house_number = buildingNumber;
+
+    return location;
+  } catch {
+    throw Error('Invalid street address.');
+  }
+};
+
+const validatePhoto = (photo) => {
+  // Make sure photo is a URL
+  if (!validator.isURL(photo)) throw Error('Photos must be valid image URLs.');
+
+  // Make sure it is a PNG or JPG
+  const re = /.(png|jpe?g)$/i;
+  if (!photo.match(re)) throw Error('Photos must be either PNGs or JP(E)Gs.');
+};
+
 export default {
   validateEmail,
   validateName,
   validateAvatar,
-  validateBio
+  validateBio,
+  validateDesc,
+  validateAddress,
+  validatePhoto
 };
