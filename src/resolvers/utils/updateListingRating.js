4ac0de952979c8605ec5ca4c65a11dda4f9b3529
jmtes <juliennetesoro@gmail.com>
2020-10-08 18:46:19 -0700

    Create utility for updating listing rating For use in review mutations

---
+++ src/resolvers/utils/updateListingRating.js
@@ -0,0 +1,22 @@
+const updateListingRating = async (review, listingId, mutation, prisma) => {
+  const listing = await prisma.query.listing(
+    { where: { id: listingId } },
+    '{ ratingSum reviewCount rating }'
+  );
+
+  const data = {
+    ratingSum: listing.ratingSum,
+    reviewCount: listing.reviewCount,
+    rating: listing.rating
+  };
+
+  if (mutation === 'CREATE') {
+    data.ratingSum += review.rating;
+    data.reviewCount += 1;
+    data.rating = (data.ratingSum / data.reviewCount).toFixed(2);
+
+    await prisma.mutation.updateListing({ where: { id: listingId }, data });
+  }
+};
+
+export default updateListingRating;
